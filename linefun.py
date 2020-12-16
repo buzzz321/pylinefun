52e5c692ca9798b1d7649d3cc5c68c7ce1b71932
Anders Olme <anders.olme@gmail.com>
2020-12-16 21:13:02 +0100

    now with some interesting patterns too

--- linefun.py
+++ linefun.py
@@ -1,29 +1,50 @@
-import sys, pygame, pygame.gfxdraw, math
+import sys, pygame, pygame.gfxdraw
+from math import *
+
 pygame.init()
 
 size = width, height = 1200, 1000
 speed = [2, 2]
 black = 0, 0, 0
 colour = (255, 0, 0)
+colour2 = (0, 255, 0)
 
 posX = width / 2
 posY = height / 2
+pos2X = width / 2
+pos2Y = height / 2
+
+pos2X += 30
+pos2Y += 30
 
 screen = pygame.display.set_mode(size)
 
+incX = 0
+incY = 0
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    pygame.gfxdraw.pixel(screen, int(posX), int(posY), colour)
+    screen.fill(black)
+    #pygame.gfxdraw.pixel(screen, int(posX), int(posY), colour)
+    #pygame.gfxdraw.pixel(screen, int(pos2X), int(pos2Y), colour2)
 
-    posX += speed[0]
-    posY += speed[1]
+    pygame.gfxdraw.line(screen, int(posX), int(posY), int(pos2X), int(pos2Y),
+                        colour)
 
-    if posX < 0 or posX > width:
-        speed[0] = -speed[0]
-    if posY < 0 or posY > height:
-        speed[1] = -speed[1]
+    posX = posX + sin(radians(incX / 10.0)) * 1.4 * sin(radians(incY / 45.9))
+    posY = posY + cos(radians(incY / 10.0)) * 1.4 * sin(radians(incX / 45.0))
 
-    #screen.fill(black)
+    pos2X = pos2X + sin(radians(incX / 5.0)) * 1.2 * sin(radians(incY / 40.9))
+    pos2Y = pos2Y + cos(radians(incY / 5.0)) * 1.2 * sin(radians(incX / 40.0))
+
+    incX += speed[0]
+    incY += speed[1]
+
+    #if posX < 0 or posX > width:
+    #    posX = width / 2
+
+    #if posY < 0 or posY > height:
+    #    posY = height / 2
+
     pygame.display.flip()
